64d0705cd6f7ab0b2438eef4c75825a74e1b2631
Kamil Ogórek <kamil.ogorek@gmail.com>
2020-02-11 12:44:19 +0100

    build: Move from gcc to cc crate

--- build.rs
+++ build.rs
@@ -1,18 +1,14 @@
-extern crate gcc;
+extern crate cc;
 
 use std::env;
 
 fn main() {
     let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap();
     if target_os == "windows" {
-        gcc::Build::new()
-            .file("runas-windows.c")
-            .compile("runas");
+        cc::Build::new().file("runas-windows.c").compile("runas");
         println!("cargo:rustc-link-lib=ole32");
     } else if target_os == "macos" {
-        gcc::Build::new()
-            .file("runas-darwin.c")
-            .compile("runas");
+        cc::Build::new().file("runas-darwin.c").compile("runas");
         println!("cargo:rustc-link-lib=framework=Security");
     }
 }
