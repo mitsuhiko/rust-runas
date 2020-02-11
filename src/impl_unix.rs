bf1f779b7be70df7e6e8d68ffbf4ea693b8c210f
Kamil Ogórek <kamil.ogorek@gmail.com>
2020-02-11 12:54:24 +0100

    feat: Handle missing sudo binary

--- src/impl_unix.rs
+++ src/impl_unix.rs
@@ -4,15 +4,17 @@ use std::process::ExitStatus;
 
 use Command;
 
-
 pub fn runas_impl(cmd: &Command) -> io::Result<ExitStatus> {
     let mut c = process::Command::new("sudo");
     if cmd.force_prompt {
         c.arg("-k");
     }
-    c
-        .arg("--")
-        .arg(&cmd.command)
-        .args(&cmd.args[..])
-        .status()
+
+    match c.arg("--").arg(&cmd.command).args(&cmd.args[..]).spawn() {
+        Ok(mut child) => child.wait(),
+        Err(e) => {
+            println!("Unable to spawn sudo command. Reason: {:?}", e.kind());
+            c.status()
+        }
+    }
 }
