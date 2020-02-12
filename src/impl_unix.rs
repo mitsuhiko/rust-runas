3ddc8d09c15e071e4e79bd7952e5d16f48e980c6
Kamil Ogórek <kamil.ogorek@gmail.com>
2020-02-12 12:35:20 +0100

    feat: Use which to detect if sudo is available (#4)
    * Move from gcc to cc crate
    * Use which to detect if sudo is available

--- src/impl_unix.rs
+++ src/impl_unix.rs
@@ -1,18 +1,23 @@
+extern crate which;
+
 use std::io;
 use std::process;
 use std::process::ExitStatus;
 
 use Command;
 
-
 pub fn runas_impl(cmd: &Command) -> io::Result<ExitStatus> {
-    let mut c = process::Command::new("sudo");
-    if cmd.force_prompt {
-        c.arg("-k");
+    match which::which("sudo") {
+        Ok(_) => {
+            let mut c = process::Command::new("sudo");
+            if cmd.force_prompt {
+                c.arg("-k");
+            }
+            c.arg("--").arg(&cmd.command).args(&cmd.args[..]).status()
+        }
+        Err(_) => Err(io::Error::new(
+            io::ErrorKind::NotFound,
+            "Command `sudo` not found",
+        )),
     }
-    c
-        .arg("--")
-        .arg(&cmd.command)
-        .args(&cmd.args[..])
-        .status()
 }
